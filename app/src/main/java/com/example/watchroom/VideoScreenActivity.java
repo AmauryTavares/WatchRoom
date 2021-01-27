c53fd946e2221c1ef975f6c8bb869f09e8346c5e
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-01-27 11:32:32 -0300

    layout_navegation

--- app/src/main/java/com/example/watchroom/VideoScreenActivity.java
+++ app/src/main/java/com/example/watchroom/VideoScreenActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,6 +18,92 @@ public class VideoScreenActivity extends Activity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_screen);
+
+        LinearLayout topmb = (LinearLayout) findViewById(R.id.top_menu_button);
+
+        topmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout tmb = (LinearLayout) findViewById(R.id.top_menu_button);
+                LinearLayout lmb = (LinearLayout) findViewById(R.id.left_menu_button);
+                LinearLayout rmb = (LinearLayout) findViewById(R.id.right_menu_button);
+                LinearLayout back = (LinearLayout) findViewById(R.id.top_menu_back);
+                LinearLayout rm = (LinearLayout) findViewById(R.id.top_menu);
+
+                tmb.setVisibility(View.INVISIBLE);
+                lmb.setVisibility(View.INVISIBLE);
+                rmb.setVisibility(View.INVISIBLE);
+                back.setVisibility(View.VISIBLE);
+                rm.setVisibility(View.VISIBLE);
+
+                TextView tv = (TextView) findViewById(R.id.name_text);
+                tv.requestFocus();
+
+            }
+        });
+
+        LinearLayout back_top = (LinearLayout) findViewById(R.id.top_menu_back);
+
+        back_top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout tmb = (LinearLayout) findViewById(R.id.top_menu_button);
+                LinearLayout lmb = (LinearLayout) findViewById(R.id.left_menu_button);
+                LinearLayout rmb = (LinearLayout) findViewById(R.id.right_menu_button);
+                LinearLayout back_top = (LinearLayout) findViewById(R.id.top_menu_back);
+                LinearLayout rm = (LinearLayout) findViewById(R.id.top_menu);
+
+                tmb.setVisibility(View.VISIBLE);
+                rmb.setVisibility(View.VISIBLE);
+                lmb.setVisibility(View.VISIBLE);
+                back_top.setVisibility(View.INVISIBLE);
+                rm.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        LinearLayout leftmb = (LinearLayout) findViewById(R.id.left_menu_button);
+
+        leftmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout tmb = (LinearLayout) findViewById(R.id.top_menu_button);
+                LinearLayout lmb = (LinearLayout) findViewById(R.id.left_menu_button);
+                LinearLayout rmb = (LinearLayout) findViewById(R.id.right_menu_button);
+                LinearLayout back = (LinearLayout) findViewById(R.id.left_menu_back);
+                LinearLayout rm = (LinearLayout) findViewById(R.id.left_menu);
+
+                tmb.setVisibility(View.INVISIBLE);
+                lmb.setVisibility(View.INVISIBLE);
+                rmb.setVisibility(View.INVISIBLE);
+                back.setVisibility(View.VISIBLE);
+                rm.setVisibility(View.VISIBLE);
+
+                TextView tv = (TextView) findViewById(R.id.name_text);
+                tv.requestFocus();
+
+            }
+        });
+
+        LinearLayout back_left = (LinearLayout) findViewById(R.id.left_menu_back);
+
+        back_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout tmb = (LinearLayout) findViewById(R.id.top_menu_button);
+                LinearLayout lmb = (LinearLayout) findViewById(R.id.left_menu_button);
+                LinearLayout rmb = (LinearLayout) findViewById(R.id.right_menu_button);
+                LinearLayout back_left = (LinearLayout) findViewById(R.id.left_menu_back);
+                LinearLayout rm = (LinearLayout) findViewById(R.id.left_menu);
+
+                tmb.setVisibility(View.VISIBLE);
+                rmb.setVisibility(View.VISIBLE);
+                lmb.setVisibility(View.VISIBLE);
+                back_left.setVisibility(View.INVISIBLE);
+                rm.setVisibility(View.INVISIBLE);
+
+            }
+        });
 
         LinearLayout rmb = (LinearLayout) findViewById(R.id.right_menu_button);
 
@@ -60,6 +147,25 @@ public class VideoScreenActivity extends Activity {
 
             }
         });
+
+        Button btn_config = (Button) findViewById(R.id.btn_settings);
+
+        btn_config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RoomSettingsActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
+        Button btn_out = (Button) findViewById(R.id.btn_out);
+
+        btn_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), InitialMenuActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+        
 
         ArrayList<Video> videos = new ArrayList<>();
         AdapterVideoMenuList adapter = new AdapterVideoMenuList(this, videos);
