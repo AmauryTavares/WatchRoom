9e8eb8fb71f27c4c9676d1fec551c68f9fef45d6
Maik Luiz Paixão <paixomaik@outlook.com.br>
2021-01-31 14:08:36 -0300

    backend config

--- app/src/main/java/com/example/watchroom/LoginScreenActivity.java
+++ app/src/main/java/com/example/watchroom/LoginScreenActivity.java
@@ -3,8 +3,11 @@ package com.example.watchroom;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.parse.ParseObject;
 
 import androidx.annotation.Nullable;
 
@@ -13,7 +16,17 @@ public class LoginScreenActivity extends Activity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        ParseObject firstObject = new ParseObject("FirstClass");
+        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        firstObject.saveInBackground(e -> {
+            if (e != null){
+                Log.e("MainActivity", e.getLocalizedMessage());
+            }else{
+                Log.d("MainActivity","Object saved.");
+            }
+        });
     }
+
 
     public void Login(View view) {
         Intent intent = new Intent(view.getContext(), InitialMenuActivity.class);
