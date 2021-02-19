781e556e7ac9ad6381bfa0682cbc33dec20e8dc7
Maik Paixão <paixomaik5@gmail.com>
2021-02-19 09:40:14 -0300

    Add files via upload

--- app/src/main/java/com/example/watchroom/AdapterFriendsList.java
+++ app/src/main/java/com/example/watchroom/AdapterFriendsList.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -21,10 +22,10 @@ public class AdapterFriendsList extends ArrayAdapter<String> {
         String user = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.friends_right_menu_item, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.friends_list_item, parent, false);
         }
         // Lookup view for data population
-        @SuppressLint("WrongViewCast") Button tvName = (Button) convertView.findViewById(R.id.friends_list_menu_item);
+        TextView tvName = (TextView) convertView.findViewById(R.id.btn_friends_list);
         // Populate the data into the template view using the data object
         tvName.setText(user);
         // Return the completed view to render on screen
